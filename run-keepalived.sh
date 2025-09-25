65f4adf8017fe7e0e5b368d9cc4d517b3beb8a0e
Shannon Carver <243236+kwiksand@users.noreply.github.com>
2025-09-25 23:57:51 +0800

    Update, allow IPv6

--- run-keepalived.sh
+++ run-keepalived.sh
@@ -79,7 +79,7 @@ for vip in $VIP_ADDRESSES; do
   if [[ $vip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     VIP_ADDRESSES_IPV4="$VIP_ADDRESSES_IPV4 $vip"
     export FOUND_IPv4=true
-  elif [[ $vip =~ ^[0-9a-fA-F:]+$ ]] || [[ $ip == "::1" ]]; then
+  elif [[ $vip =~ ^[0-9a-fA-F:]+$ ]] || [[ $vip == "::1" ]]; then
     VIP_ADDRESSES_IPV6="$VIP_ADDRESSES_IPV6 $vip"
     export FOUND_IPv6=true
   else
